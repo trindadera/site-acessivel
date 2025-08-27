f8ef1df132d52fca110f8537f9e5729ee0ddd63d
rayssa <00001112064679sp@al.educacao.sp.gov.br>
2025-08-27 09:49:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,6 @@ document.addEventListener('DOMContentLoaded', function(){
  })
  
  ScrollReveal().reveal('#inicio', { delay: 500 });
- ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ ScrollReveal().reveal('#colodedeus', { delay: 500 });
  ScrollReveal().reveal('#galeria', { delay: 500 });
  ScrollReveal().reveal('#contato', { delay: 500 });
